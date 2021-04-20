8b1e76503147227d3c34c6d89139a8131183fa39
John Ketterer <johnmketterer@gmail.com>
2021-04-19 22:04:24 -0500

    found and updated array in a document

--- myApp.js
+++ myApp.js
@@ -68,18 +68,12 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = 'hamburger';
-
-  // .findById() method to find a person by _id with the parameter personId as search key. 
-  Person.findById(personId, (err, person) => {
-    if(err) return console.log(err); 
-  
-    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
-    person.favoriteFoods.push(foodToAdd);
-
-    // and inside the find callback - save() the updated Person.
-    person.save((err, updatedPerson) => {
+  Person.findById(personId, (err, data) => {
+    if(err) return console.log(err);   
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, updatedData) => {
       if(err) return console.log(err);
-      done(null, updatedPerson)
+      done(null, updatedData)
     })
   })
 };
