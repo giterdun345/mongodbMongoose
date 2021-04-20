2c7af5b7f00beef5ce42119a5b097c1facfb7ea2
John Ketterer <johnmketterer@gmail.com>
2021-04-19 20:21:17 -0500

    schema added

--- myApp.js
+++ myApp.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 require('mongoose')
-console.log(process.env.MONGO_URI)
+// console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const PersonSchema = new mongoose.Schema({
+  name:{type: String, required:true},
+  age: Number,
+  favoriteFoods: [String]
+})
 
 let Person;
 
